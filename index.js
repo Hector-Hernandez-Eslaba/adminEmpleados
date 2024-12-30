f762f626d68114804c234fcc426b2b1e0e0e4b44
Skraken <hheslaba@gmail.com>
2024-12-30 13:39:26 -0600

    Update css javascript

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let empleados = [];
 
-function Trabajador(nombre, apellido1, apellido2, anios, puesto){
+function Trabajador(nombre, apellido1, apellido2, edad, puesto){
     this.nombre = nombre,
     this.apellido1 = apellido1,
     this.apellido2 = apellido2,
-    this.anios = anios,
+    this.edad = edad,
     this.puesto = puesto
 }
 
@@ -17,7 +17,10 @@ function enviarDatos(){
 
     let trabajadores = new Trabajador(nombre, apellido1, apellido2, edad, puesto);
     empleados.push(trabajadores)
-    console.log(trabajadores)
+    
+    alert("Se Capturaron las datos correctamente");
+    
+    limpiarDatos();
 
 }
 
@@ -25,16 +28,31 @@ const obrero = document.querySelector(".boxCards");
 console.log(obrero);
 
 function mostrarEmpleados(){
+    const masterCard = document.createElement("div")
+    masterCard.className = "masterCard"
+    masterCard.innerHTML = `
+    <img class="imgCard" src="Imagenes/Imagen.png" alt="imagenCard">
+    `
+    const textCard = document.createElement("div");
+    
     for(let arrayEmpleados of empleados){
         for(let mostrarTrabajadores in arrayEmpleados){
+            
+            const obreroCard = document.createElement("p")
+            obreroCard.innerText = mostrarTrabajadores + ": " + arrayEmpleados[mostrarTrabajadores];
 
-            let obreroCard = document.createElement("p")
-            obreroCard.innerText = arrayEmpleados.nombre
-
-            obrero.appendChild(obreroCard);
-            //alert(mostrarTrabajadores + arrayEmpleados[mostrarTrabajadores])
-
+            console.log(mostrarTrabajadores)
+            obrero.appendChild(masterCard)
+            masterCard.appendChild(textCard);
+            textCard.appendChild(obreroCard);            
         }
     }
 }
 
+function limpiarDatos(){
+    document.querySelector("#nombre").value = "";
+    document.querySelector("#apellido1").value = "";
+    document.querySelector("#apellido2").value = "";
+    document.querySelector("#edad").value = "";
+    document.querySelector("#puesto").value = "";
+}
