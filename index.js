f392c9cedb528bf20510febdeb5f07dba502a4e8
Hector-Hernandez-Eslaba <157669650+Hector-Hernandez-Eslaba@users.noreply.github.com>
2024-12-29 23:03:07 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let empleados = [];
+
+function Trabajador(nombre, apellido1, apellido2, anios, puesto){
+    this.nombre = nombre,
+    this.apellido1 = apellido1,
+    this.apellido2 = apellido2,
+    this.anios = anios,
+    this.puesto = puesto
+}
+
+function enviarDatos(){
+    let nombre = document.querySelector("#nombre").value;
+    let apellido1 = document.querySelector("#apellido1").value;
+    let apellido2 = document.querySelector("#apellido2").value;
+    let edad = document.querySelector("#edad").value;
+    let puesto = document.querySelector("#puesto").value;
+
+    let trabajadores = new Trabajador(nombre, apellido1, apellido2, edad, puesto);
+    empleados.push(trabajadores)
+    console.log(trabajadores)
+
+}
+
+function mostrarEmpleados(){
+    for(let arrayEmpleados of empleados){
+        for(let mostrarTrabajadores in arrayEmpleados){
+            alert(mostrarTrabajadores + arrayEmpleados[mostrarTrabajadores])
+
+        }
+    }
+}
+
